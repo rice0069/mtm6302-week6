08d99f2ddcfc7b3dd7f31e7f43a67741899f828a
Marc Rice <rice0069@algonquinlive.com>
2023-06-16 09:07:28 -0400

    creates starting files

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body")
+
+bodyTag.innerHTML += `
+    <section id="groceries"> 
+        <h2>Groceries</h2>
+        <ul id="groceryList"></ul>
+    </section>
+`
