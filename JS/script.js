8f92e8a4a79f61e5a67a4eea7567929380eb2d45
Marc Rice <rice0069@algonquinlive.com>
2023-06-16 10:15:00 -0400

    completes in class demonstration

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,6 @@
-const body = document.querySelector("body")
+console.log("Js File Added")
+
+const bodyTag = document.querySelector("body")
 
 bodyTag.innerHTML += `
     <section id="groceries"> 
@@ -6,3 +8,24 @@ bodyTag.innerHTML += `
         <ul id="groceryList"></ul>
     </section>
 `
+
+let groceries = ["apple", "dog food", "bread", "bananas", "milk", "eggs"]
+const groceryUl = document.getElementById("groceryList")
+
+// for what coming from where
+for (groceryItem of groceries) {
+    groceryUl.insertAdjacentHTML("beforeend", `<li>${groceryItem}</li>`)
+}
+
+function addItem(item) {
+    if (item !== "") {
+        let newItem = `<li>${item}</li>`
+        groceryUl.insertAdjacentHTML("afterbegin", newItem)
+    }
+}
+
+const section = document.createElement("section")
+section.id = "todo"
+section.innerHTML = "<h2>Todo List</h2>"
+
+bodyTag.appendChild(section)
